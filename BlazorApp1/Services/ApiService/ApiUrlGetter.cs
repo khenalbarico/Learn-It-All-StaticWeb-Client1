59ba959718bcca459d4e69256d14b5ab139b4add
khenalbarico05 <khenalbarico05@gmail.com>
2026-07-03 17:51:49 +0800

    dev endpoint fix2

--- BlazorApp1/Services/ApiService/ApiUrlGetter.cs
+++ BlazorApp1/Services/ApiService/ApiUrlGetter.cs
@@ -8,7 +8,7 @@ public class ApiUrlGetter : IApiUrlGetter
 
         return env switch
         {
-            "development" => "https://localhost:7041/api/",
+            "development" => "http://localhost:7041/api/",
             "production"  => "https://learn-it-all-api-dev1.azurewebsites.net/api/",
             _             => throw new ArgumentException($"Unknown environment: {env}")
         };
